dcb77a02408d6915c87da276786d1ef3e49fcdd0
st-quando <quan.do@supremetech.vn>
2020-05-04 11:46:21 +0700

    dev

--- src/components/home/skill-design.vue
+++ src/components/home/skill-design.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <v-row class="pt-10 pb-10 f-row">
+    <v-row class="pt-10 pb-10 f-row f-center-x">
       <v-col sm="6" cols="12" class="pl-6 pr-6">
-        <img src="../../assets/img/about-us/8.png">
+        <img class="default-img" src="../../assets/img/about-us/8.png">
       </v-col>
       <v-col sm="6" cols="12" class="pl-6 pr-6">
-        <span class="yellow-label">Technical</span>
+        <span class="badge badge-primary">Technical</span>
         <h2>We Have Multiple Skills</h2>
         <p>We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.We use the latest technologies it voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
         <v-row no-gutters class="f-row skill-percent mt-4">
-          <v-col sm="4" cols="12">
-            <div class="percent">80<span>%</span></div>
+          <v-col cols="3">
+            <div class="percent">99<span>%</span></div>
             <span>MEAN</span>
           </v-col>
-          <v-col sm="4" cols="12">
-            <div class="percent">60<span>%</span></div>
+          <v-col cols="3">
+            <div class="percent">99<span>%</span></div>
             <span>LAMP</span>
           </v-col>
-          <v-col sm="4" cols="12">
-            <div class="percent">70<span>%</span></div>
+          <v-col cols="3">
+            <div class="percent">99<span>%</span></div>
             <span>WP</span>
+          </v-col>
+          <v-col cols="3">
+            <div class="percent">99<span>%</span></div>
+            <span>Other</span>
           </v-col>
         </v-row>
       </v-col>
