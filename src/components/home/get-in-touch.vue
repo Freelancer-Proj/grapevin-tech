6275e8ec80e146c0b2ddb8cee48a3f08df9902e8
at-trangle <trang.le2@asiantech.vn>
2020-05-10 22:28:21 +0700

    fix comment

--- src/components/home/get-in-touch.vue
+++ src/components/home/get-in-touch.vue
@@ -4,28 +4,28 @@
       <v-col sm="6" cols="12" class="pl-4 pr-4">
         <div class="contact-form pd-5">
           <h2 class="mb-5">Contact us</h2>
-          <v-form v-model="valid">
+          <v-form v-model="contact.valid">
             <v-row class="form-group">
               <v-col sm="6" cols="12" class="pt-0 pb-0 pr-2">
-                <v-text-field v-model="name" :rules="nameRules" label="名前" outlined required></v-text-field>
+                <v-text-field v-model="contact.name" :rules="contact.nameRules" label="名前" filled required></v-text-field>
               </v-col>
               <v-col sm="6" cols="12" class="pt-0 pb-0 pl-2">
-                <v-text-field v-model="company" :rules="companyRules" label="会社名" outlined required></v-text-field>
+                <v-text-field v-model="contact.company" :rules="contact.companyRules" label="会社名" filled required></v-text-field>
               </v-col>
             </v-row>
             <v-row class="form-group">
               <v-col sm="12" cols="12" class="pt-0 pb-0">
-                <v-text-field v-model="phoneNumber" :rules="phoneNumberRules" :type="'number'" label="電話番号" outlined required></v-text-field>
+                <v-text-field v-model="contact.phoneNumber" :rules="contact.phoneNumberRules" :type="'number'" label="電話番号" filled required></v-text-field>
               </v-col>
             </v-row>
             <v-row class="form-group">
               <v-col sm="12" cols="12" class="pt-0 pb-0">
-                <v-text-field v-model="email" :rules="emailRules" label="メールアドレス" outlined required></v-text-field>
+                <v-text-field v-model="contact.email" :rules="contact.emailRules" label="メールアドレス" filled required></v-text-field>
               </v-col>
             </v-row>
             <v-row class="form-group">
               <v-col sm="12" cols="12" class="pt-0 pb-0">
-                <v-textarea v-model="content" :rules="contentRules" label="お問い合わせ内容" rows="4" outlined required></v-textarea>
+                <v-textarea v-model="contact.content" :rules="contact.contentRules" label="お問い合わせ内容" rows="4" filled required></v-textarea>
               </v-col>
             </v-row>
             <div class="txt-center">
@@ -58,28 +58,30 @@ export default {
   },
   data() {
     return {
-      valid: false,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-      ],
-      company: '',
-      companyRules: [
-        v => !!v || 'Company is required',
-      ],
-      phoneNumber: '',
-      phoneNumberRules: [
-        v => !!v || 'Phone numer is required',
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid',
-      ],
-      content: '',
-      contentRules: [
-        v => !!v || 'Phone numer is required',
-      ],
+      contact: {
+        valid: false,
+        name: '',
+        nameRules: [
+          v => !!v || 'Name is required',
+        ],
+        company: '',
+        companyRules: [
+          v => !!v || 'Company is required',
+        ],
+        phoneNumber: '',
+        phoneNumberRules: [
+          v => !!v || 'Phone numer is required',
+        ],
+        email: '',
+        emailRules: [
+          v => !!v || 'E-mail is required',
+          v => /.+@.+/.test(v) || 'E-mail must be valid',
+        ],
+        content: '',
+        contentRules: [
+          v => !!v || 'Phone numer is required',
+        ],
+      },
       contactData: [
         {
           icon: 'contacts',
