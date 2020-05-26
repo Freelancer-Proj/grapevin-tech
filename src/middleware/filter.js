2974a484eb3ea9042e805f840968c90097255354
at-trangle <trang.le2@asiantech.vn>
2020-05-26 17:23:19 +0700

    issue 33 refactore portflio

--- src/middleware/filter.js
+++ src/middleware/filter.js
@@ -5,7 +5,18 @@ const filterPlugin = {
   install() {
     Vue.filter('dateTime', (value, formatDate = 'dd MMMM yyyy') => {
       return format(value, formatDate)
-    })
+    });
+    Vue.filter('shortDateTime', (value, formatDate = 'dd MMMM') => {
+      return format(value, formatDate)
+    });
+    Vue.filter('shortDesc', (string) => {
+      console.log(string);
+      if (string.length > 150) {
+        return string.slice(0, 150) + '...';
+      } else {
+        return string;
+      }
+    });
   },
 };
 
