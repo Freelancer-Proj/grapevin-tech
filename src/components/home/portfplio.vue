406b214ef507897073d3b70c7845ef70500a0508
at-trangle <trang.le2@asiantech.vn>
2020-06-01 18:23:28 +0700

    add dialog component

--- src/components/home/portfplio.vue
+++ src/components/home/portfplio.vue
@@ -17,104 +17,36 @@
           <h4 class="">{{item.name}}</h4>
           <p class="pt-4 pl-4 pr-4">{{ item.description | shortDesc }}</p>
           <!-- <p v-html="short(item.description)"></p> -->
-          <button class="btn btn-primary">View More</button>
+          <button class="btn btn-primary" @click="showDialog(item.id)">View More</button>
         </div>
       </v-col>
-      <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-        abc
+      <v-dialog v-model="isPortfolioDialogShow" fullscreen hide-overlay transition="dialog-bottom-transition">
+        <Dialog :portfplioData="curentPorrtfolio"/>
       </v-dialog>
-      
     </v-row>
-    <v-row justify="center">
-    <v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
-      <template v-slot:activator="{ on }">
-        <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-      </template>
-      <v-card>
-        <v-toolbar dark color="primary">
-          <v-btn icon dark @click="dialog = false">
-            <v-icon>mdi-close</v-icon>
-          </v-btn>
-          <v-toolbar-title>Settings</v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-toolbar-items>
-            <v-btn dark text @click="dialog = false">Save</v-btn>
-          </v-toolbar-items>
-        </v-toolbar>
-        <v-list three-line subheader>
-          <v-subheader>User Controls</v-subheader>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Content filtering</v-list-item-title>
-              <v-list-item-subtitle>Set the content filtering level to restrict apps that can be downloaded</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title>Password</v-list-item-title>
-              <v-list-item-subtitle>Require password for purchase or use password to restrict purchase</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-        <v-divider></v-divider>
-        <v-list three-line subheader>
-          <v-subheader>General</v-subheader>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="notifications"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Notifications</v-list-item-title>
-              <v-list-item-subtitle>Notify me about updates to apps or games that I downloaded</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="sound"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Sound</v-list-item-title>
-              <v-list-item-subtitle>Auto-update apps at any time. Data charges may apply</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-action>
-              <v-checkbox v-model="widgets"></v-checkbox>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Auto-add widgets</v-list-item-title>
-              <v-list-item-subtitle>Automatically add home screen widgets</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-card>
-    </v-dialog>
-  </v-row>
   </div>
 </div>
 </template>
 <script>
-// import Vue from 'vue'
+import Dialog from '../portfplio/dialog';
 
 export default {
   components: {
+    Dialog
   },
-  // methods: {
-  //   short: (val) => {
-  //     console.log(Vue.filter('shortDesc'));
-  //     return this.$options.filters.shortDesc(val);
-  //   }
-  // },
+  methods: {
+    showDialog(id) {
+      this.isPortfolioDialogShow = true;
+      this.curentPorrtfolio = this.portfplioData.find(x => x.id === id);
+    }
+  },
   data() {
     return {
-      dialog: false,
-      dialog: false,
-      notifications: false,
-      sound: true,
-      widgets: false,
+      isPortfolioDialogShow: false,
+      curentPorrtfolio: Object,
       portfplioData: [
         {
-          dialog: false,
+          id: 1,
           name: 'ライブ配信/生放送アプリ',
           images: require('~/assets/img/portfplio/1.jpg'),
           date: new Date(),
@@ -124,7 +56,7 @@ export default {
                         Wingtrillは、チャットウィンドウであなたのライブビデオストリームにコメントするためのオプションを友人に提供します。 友人が何らかの形でライブビデオストリームを見逃していると、後で24時間まで記録されたコピーを見ることができます。`
         },
         {
-          dialog: false,
+          id: 2,
           name: 'ECアプリ（体験型）',
           images: require('~/assets/img/portfplio/2.jpg'),
           date: new Date(),
@@ -133,7 +65,7 @@ export default {
                         試して購入：実際には、買い手がさまざまな服を試して、オンライン上でマネキンに合わせて、フィッティング、色、スタイルなどを評価するのに役立つ非常に新しくユニークなテクニックです。 バイヤーを美化することに関連しています。 My Kool Lookドレッシングルームをチェックしたり、写真を共有したりして、お買い物をお楽しみになれます。 これは、躊躇せずに、最高かつ革新的な技術の一つです。`
         },
         {
-          dialog: false,
+          id: 3,
           name: 'eラーニングサイト',
           images: require('~/assets/img/portfplio/3.jpg'),
           date: new Date(),
---
+++ src/components/portfplio/dialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-row class="f-row f-center-x">
+      <v-col sm="6" cols="12" class="pd-5">
+        <no-ssr>
+          <slick ref="slick">
+            <div class="about-us-box" v-for="(item, index) of portfplioData.images" :key="index">
+              <img :src="item.url" alt="portfplio image">
+            </div>
+          </slick>
+        </no-ssr>
+      </v-col>
+      <v-col sm="6" cols="12" class="pd-5">
+        <p v-html="portfplioData.description"></p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+<script>
+
+export default {
+  components: {
+  },
+  props: {
+    portfplioData: Object,
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
