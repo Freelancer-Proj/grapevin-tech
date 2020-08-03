85ef7c078c88d200d0c9395592be9b964295e69a
at-trangle <trang.le2@asiantech.vn>
2020-08-03 10:07:01 +0700

    implement api portfplio

--- src/components/home/portfplio.vue
+++ src/components/home/portfplio.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div v-if="portfolioData">
     <div class="txt-center">
       <!-- <span class="btn btn-green txt-bold">What We Offerd</span> -->
       <h2 class="mt-3 mb-2">Portfolio</h2>
@@ -11,12 +11,11 @@
           class="pd-5"
           v-for="(item, index) of portfolioData" :key="index">
           <div class="portfolio-content" :class="{ 'portfolio-center' : index === 1 }">
-            <img :src="item.images[0]" alt="">
-            <span>{{item.type}}</span>
-            <span class="portfolio-time pd-2">{{ item.date | dateTime('MM yyyy') }}</span>
-            <h4 class="">{{item.name}}</h4>
-            <p class="pt-4 pl-4 pr-4">{{ item.description | HTMLtoText | shortDesc(100) }}</p>
-            <!-- <p v-html="short(item.description)"></p> -->
+            <img v-if="item.listImg.length" :src="item.listImg[0]" alt="">
+            <span>{{item.type_portfolio}}</span>
+            <span class="portfolio-time pd-2">{{ new Date(item.updated_at) | dateTime('MM yyyy') }}</span>
+            <h4 class="">{{item.title}}</h4>
+            <p class="pt-4 pl-4 pr-4">{{ item.desccription | HTMLtoText | shortDesc(100) }}</p>
             <div class="pt-4 pl-4 pr-4 pb-4">
               <button class="btn btn-primary" @click.stop="showDialog(item.id)">View More</button>
             </div>
@@ -28,7 +27,22 @@
 </template>
 <script>
 
+import {api} from '../../helpers/services/api.service'
+
 export default {
+  created() {
+    api.get(['portfolios']).then(res => {
+      if (res) {
+        res.map(x => {
+          if (x.cover) {
+            x.listImg = [x.cover.url];
+          }
+          x.desccription = this.$options.filters.shortDesc(x.desc);
+        })
+        this.portfolioData = res;
+      }
+    });
+  },
   methods: {
     showDialog(id) {
       this.currentPortfolio = this.portfolioData.find(x => x.id === id)
@@ -53,54 +67,10 @@ export default {
       dialog: false,
       isPortfolioDialogShow: false,
       currentPortfolio: Object,
-      portfolioData: [
-        {
-          id: 1,
-          name: 'ライブ配信/生放送アプリ',
-          images: [
-            require('~/assets/img/portfplio/1.jpg'),
-            require('~/assets/img/portfplio/2.jpg'),
-            require('~/assets/img/portfplio/3.jpg')
-          ],
-          date: new Date(),
-          type: 'Sass',
-          description: `Wingtrillは、いつでも、どこでも、あなたの人生の生き生きとした重要な瞬間をあなたの友人や家族と共有するのに役立ちます。
-                        ライブビデオストリームをブロードキャストすると、Facebookの友人、ツイッターフォロワー、ローカルアドレス帳の連絡先に招待状を送ってライブビデオストリームを視聴できるようになります。<br>
-                        Wingtrillは、チャットウィンドウであなたのライブビデオストリームにコメントするためのオプションを友人に提供します。 友人が何らかの形でライブビデオストリームを見逃していると、後で24時間まで記録されたコピーを見ることができます。`
-        },
-        {
-          id: 2,
-          name: 'ECアプリ（体験型）',
-          images: [
-            require('~/assets/img/portfplio/2.jpg'),
-            require('~/assets/img/portfplio/1.jpg'),
-            require('~/assets/img/portfplio/3.jpg')
-          ],
-          date: new Date(),
-          type: 'Marketing',
-          description: `特定の洋服をオンラインで購入するかどうかを決定するだけで、多くの時間が失われていますか？ この問題は、オンラインドレッシングルームの出現により、解決されており、お気に入りの洋服を購入することができます。 このアプリは、顧客がスムーズに買い物をするのを手伝うための媒体の一つとして提供されます。<br>
-                        試して購入：実際には、買い手がさまざまな服を試して、オンライン上でマネキンに合わせて、フィッティング、色、スタイルなどを評価するのに役立つ非常に新しくユニークなテクニックです。 バイヤーを美化することに関連しています。 My Kool Lookドレッシングルームをチェックしたり、写真を共有したりして、お買い物をお楽しみになれます。 これは、躊躇せずに、最高かつ革新的な技術の一つです。`
-        },
-        {
-          id: 3,
-          name: 'eラーニングサイト',
-          images: [
-            require('~/assets/img/portfplio/3.jpg'),
-            require('~/assets/img/portfplio/1.jpg'),
-            require('~/assets/img/portfplio/2.jpg')
-          ],
-          date: new Date(),
-          type: 'Landing',
-          description: `OpenSesameはiTunesから楽曲をダウンロードするのと同じくらい簡単にeラーニングコースを購入し販売します。 OpenSesameがコースをプレビューしたり、レビューを読んで研究したりします。 サブスクリプションや長期コミットメントのないコースを購入し、LMS（Learning Management System）で即座に使用できます。 OpenSesameが協力し、従業員にとって最高のコース選びに集中できます。`
-        }
-      ]
+      portfolioData: Array
     }
   },
   mounted() {
-    this.portfolioData = this.portfolioData.map(x => ({
-      ...x,
-      description: this.$options.filters.shortDesc(x.description)
-    }))
   }
 }
 </script>
