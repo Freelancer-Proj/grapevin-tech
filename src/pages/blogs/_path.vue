c3fc40294974db5fec4ac0f1dde528af2a9e7fb4
at-trangle <trang.le2@asiantech.vn>
2020-09-12 09:09:01 +0700

    fix blog detail description

--- src/pages/blogs/_path.vue
+++ src/pages/blogs/_path.vue
@@ -23,7 +23,7 @@
           </div> -->
         </div>
         <h2 class="mb-3">{{ blogDetail.title }}</h2>
-        <p class="blog-desc" v-html="blogDetail.content"></p>
+        <p class="blog-desc" v-html="blogDetail.desc"></p>
         <h4 class="blog-time mb-2">時間: </h4>{{ blogDetail.updated_at | dateTime('dd MMMM yyyy') }}
         <div class="blog-share-tag mt-4">
           <div class="share-social">
